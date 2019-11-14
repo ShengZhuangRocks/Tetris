b0e86c091acf43865fae33fbabe4f97d0c926775
ShengZhuang <shengzh9@gmail.com>
2019-11-14 20:20:06 +1000

    add DWYTPL liscense

--- tetris.py
+++ tetris.py
@@ -1,3 +1,20 @@
+"""
+DO WHATEVER YOU WANT TO PUBLIC LICENSE
+Version 2, December 2004
+ 
+Copyright (C) 2019 Sheng Zhuang <shengzh9@gmail.com>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+ 
+DO WHATEVER YOU WANT TO PUBLIC LICENSE
+TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+ 0. You just DO WHATEVER YOU WANT TO.
+"""
+
+
 import random, math, pygame
 from pygame.locals import *
 from brick import BrickPattern
